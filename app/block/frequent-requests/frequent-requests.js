40d094a51d5b7846340953840ae33c335533c6c4
Vladislav <wizardhype1998@gmail.com>
2020-05-06 18:16:41 +0400

    поменля let на var

--- app/block/frequent-requests/frequent-requests.js
+++ app/block/frequent-requests/frequent-requests.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let questions = document.querySelectorAll('.frequent-requests__question')
-    let answers = document.querySelectorAll('.frequent-requests__answer')
-
-    for (let i = 0; i < questions.length; i++) {
+    var questions = document.querySelectorAll('.frequent-requests__question');
+    var answers = document.querySelectorAll('.frequent-requests__answer');
+    for (var i = 0; i < questions.length; i++) {
         questions[i].onclick = function () {
-            for (let i = 0; i < questions.length; i++) {
-                questions[i].classList.remove('frequent-requests__question_active')
-                answers[i].classList.remove('frequent-requests__answer_active')
-            }
-            this.classList.toggle('frequent-requests__question_active')
-            answers[i].classList.toggle('frequent-requests__answer_active')
-        }
-    }
-})
+            for (var i = 0; i < questions.length; i++) {
+                questions[i].classList.remove('frequent-requests__question_active');
+                answers[i].classList.remove('frequent-requests__answer_active');
+            };
+            this.classList.toggle('frequent-requests__question_active');
+            answers[i].classList.toggle('frequent-requests__answer_active');
+        };
+    };
+});
