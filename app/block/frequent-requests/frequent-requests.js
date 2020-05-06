345026ad4000532452f6545bb05ce85b6ec12df0
Vladislav <wizardhype1998@gmail.com>
2020-05-06 19:00:35 +0400

    решал проблему с js

--- app/block/frequent-requests/frequent-requests.js
+++ app/block/frequent-requests/frequent-requests.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var questions = document.querySelectorAll('.frequent-requests__question');
-    var answers = document.querySelectorAll('.frequent-requests__answer');
+    var questions = document.querySelectorAll('.frequent-requests__question')
+    var answers = document.querySelectorAll('.frequent-requests__answer')
     for (var i = 0; i < questions.length; i++) {
         questions[i].onclick = function () {
             for (var i = 0; i < questions.length; i++) {
-                questions[i].classList.remove('frequent-requests__question_active');
-                answers[i].classList.remove('frequent-requests__answer_active');
-            };
-            this.classList.toggle('frequent-requests__question_active');
-            answers[i].classList.toggle('frequent-requests__answer_active');
-        };
-    };
-});
+                questions[i].classList.remove('frequent-requests__question_active')
+                answers[i].classList.remove('frequent-requests__answer_active')
+            }
+            this.classList.toggle('frequent-requests__question_active')
+            this.nextElementSibling.classList.toggle('frequent-requests__answer_active')
+        }
+    }
+})
