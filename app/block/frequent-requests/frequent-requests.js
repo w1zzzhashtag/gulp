75b3f06fa6fdb29d80ffe0d845782b822b055c43
Vladislav <wizardhype1998@gmail.com>
2020-05-06 18:07:37 +0400

    страница Клиенты - закночил блок ЧАСТЫЕ ВОПРОСЫ, сверстал блок скачать приложение и начал футер

--- app/block/frequent-requests/frequent-requests.js
+++ app/block/frequent-requests/frequent-requests.js
@@ -1,3 +1,15 @@
-document.addEventListener('DONContentLoaded', function() {
-    console.log('hello world')
+document.addEventListener('DOMContentLoaded', function () {
+    let questions = document.querySelectorAll('.frequent-requests__question')
+    let answers = document.querySelectorAll('.frequent-requests__answer')
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].onclick = function () {
+            for (let i = 0; i < questions.length; i++) {
+                questions[i].classList.remove('frequent-requests__question_active')
+                answers[i].classList.remove('frequent-requests__answer_active')
+            }
+            this.classList.toggle('frequent-requests__question_active')
+            answers[i].classList.toggle('frequent-requests__answer_active')
+        }
+    }
 })
